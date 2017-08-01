1e2e62fb4d33e8227a11c6a28a995bc037d74e68
Alex Kassil <alexkassil@sbcglobal.net>
2017-07-31 18:50:53 -0700

    Add all functions prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akassil <akassil@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 12:05:18 by akassil           #+#    #+#             */
-/*   Updated: 2017/07/31 12:06:23 by akassil          ###   ########.fr       */
+/*   Updated: 2017/07/31 14:13:53 by akassil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define LIBFT_H
 
 int		ft_atoi(char *str);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strncat(char *dest, char *src, int nb);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+int		ft_strcmp(char *s1, char *s2);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
 
 #endif
