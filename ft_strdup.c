cfb8cc4520f5e5f3ad64c249f1c55c87cd963074
Alex Kassil <alexkassil@sbcglobal.net>
2017-08-28 00:35:02 -0700

    Add malloc protection

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: akassil <akassil@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 13:54:41 by akassil           #+#    #+#             */
-/*   Updated: 2017/08/02 01:13:11 by akassil          ###   ########.fr       */
+/*   Updated: 2017/08/28 00:33:24 by akassil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	*ft_strdup(char *str)
 
 	size = ft_strlen(str);
 	new = malloc(sizeof(char) * (size + 1));
+	if (new == NULL)
+		return (new);
 	i = 0;
 	while (str[i])
 	{
