425116bad4e59a0337163ae44a7cb6f2573d023b
Alex Kassil <alexkassil@sbcglobal.net>
2017-08-30 10:10:46 -0700

    Added ft_memset

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: akassil <akassil@student.42.us.org>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/07/31 12:00:20 by akassil           #+#    #+#              #
-#    Updated: 2017/08/02 01:11:13 by akassil          ###   ########.fr        #
+#    Updated: 2017/08/30 09:54:55 by akassil          ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 
@@ -14,7 +14,7 @@ CC=gcc
 CFLAGS=-Wall -Werror -Wextra
 NAME=libft.a
 SRCS= ft_atoi.c ft_strlen.c ft_strdup.c ft_strcpy.c ft_strncpy.c ft_strcat.c \
-      ft_strncat.c ft_strlcat.c ft_strcmp.c ft_strncmp.c
+      ft_strncat.c ft_strlcat.c ft_strcmp.c ft_strncmp.c ft_memset.c
 OBJ = $(patsubst %.c, %.o, $(SRCS))
 
 all: $(NAME)
--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: akassil <akassil@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 12:05:18 by akassil           #+#    #+#             */
-/*   Updated: 2017/07/31 14:13:53 by akassil          ###   ########.fr       */
+/*   Updated: 2017/08/30 10:01:26 by akassil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int		ft_atoi(char *str);
+long		ft_atoi(char *str);
 int		ft_strlen(char *str);
 char	*ft_strdup(char *str);
 char	*ft_strcpy(char *dest, char *src);
@@ -22,7 +22,7 @@ char	*ft_strcat(char *dest, char *src);
 char	*ft_strncat(char *dest, char *src, int nb);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 int		ft_strcmp(char *s1, char *s2);
-int	ft_strncmp(char *s1, char *s2, unsigned int n);
-
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
+void    *ft_memset(void *s, int c, int n);
 
 #endif
