0ec04bc4947ebbe68c65a7f6144fa859b2b1701a
Alex Kassil <akassil@e1z1r4p28.42.us.org>
2018-05-02 16:39:50 -0700

    Fix style

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,23 +6,31 @@
 /*   By: akassil <akassil@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 22:07:38 by akassil           #+#    #+#             */
-/*   Updated: 2018/05/02 16:02:21 by akassil          ###   ########.fr       */
+/*   Updated: 2018/05/02 16:34:06 by akassil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static	int		is_ws(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\r' || c == '\v' || c == ' '
+		|| c == '\f')
+		return (1);
+	return (0);
+}
+
+char			*ft_strtrim(char const *s)
 {
 	int		i;
 	int		l;
 	char	*r;
 
 	l = ft_strlen((char *)s);
-	while (is_whitespace(s[l - 1]))
+	while (is_ws(s[l - 1]))
 		l--;
 	i = -1;
-	while (is_whitespace(s[++i]))
+	while (is_ws(s[++i]))
 		l--;
 	if (l <= 0)
 		return (ft_strnew(0));
